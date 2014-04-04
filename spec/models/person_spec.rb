2da91cb27679e7ebe5c4883c2ba50fd7c22c2f76
davidmiller11 <davidmiller11@gmail.com>
2014-04-04 17:26:18 -0400

    wrote all rspec tests, updated Gemfile with rails-rspec gem in dev and test env's

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,47 @@
+# person_spec.rb
+require 'spec_helper'
+
+describe Person do
+  context 'when a Person is created' do
+    describe '#name' do
+      xit 'has a full name'
+    end
+    describe '#birthday' do
+      xit 'has a birthday'
+    end
+    describe '#have_a_drink' do
+      context 'if person is over 21' do
+        xit "increases a person's drinks attr by 1" do
+        end
+        xit "doesn't allow more than 3 drinks and returns string 'Go home you're drunk"
+      end
+      context 'if person is under 21' do
+        xit "returns a string 'wait a few years'" do
+        end
+      end
+    end
+    describe '#drive_a_car' do
+      context 'if user is under 18' do
+        xit "returns a string 'not yet youngin'" do
+        end
+      end
+      context 'if user is over 18 and has license' do
+        xit "allows person to drive" do
+        end
+      end
+      context 'if person is over 21, has license but is drunk' do
+        xit "returns string 'looks like a cab for you tonight'"
+      end
+    end
+    describe '#sober_up' do
+      context 'if person has had drinks' do
+        xit 'decreases drinks attr by 1' do
+        end
+      end
+      context 'if person has 0 drinks' do
+        xit 'does not change drinks attr' do
+        end
+      end
+    end
+  end
+end
